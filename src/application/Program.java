ffd5e563b2fd288f7c3384e7d107099ef52c84f6
GustavoFlorenci0 <florencio152@gmail.com>
2023-02-03 18:30:09 -0300

    Movimentos do rei

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,6 +23,10 @@ public class Program {
 				System.out.print("source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				
 				System.out.println();
 				System.out.print("target: ");
 				ChessPosition target = UI.readChessPosition(sc);
